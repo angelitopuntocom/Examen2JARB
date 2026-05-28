b2082987d23f3a22b886185ac7471d9ef9af6353
Yaelglz-inf <nvgs502@gmail.com>
2026-05-27 20:55:53 -0600

    Diseño de vista de inicio de sesión

--- app/src/main/java/com/example/examen2jarb/MainActivity.kt
+++ app/src/main/java/com/example/examen2jarb/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.examen2jarb
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                Toast.makeText(this, "¡Bienvenido, $username!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Por favor, completa todos los campos.", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
